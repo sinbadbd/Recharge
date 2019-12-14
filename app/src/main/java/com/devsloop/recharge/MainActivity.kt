c36c620e35715c5ae9e6c6c61feefd4f7de2da75
sinbad <imufun2@gmail.com>
2019-12-15 01:15:36 +0600

    add loading

--- app/src/main/java/com/devsloop/recharge/MainActivity.kt
+++ app/src/main/java/com/devsloop/recharge/MainActivity.kt
@@ -2,19 +2,43 @@ package com.devsloop.recharge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Menu
 import android.view.MenuItem
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
-
+    private var progressBar: ProgressBar? = null
+    private var i = 0
+    private val handler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         webViewProcess()
+//        i = progressBar!!.progress
+//        Thread(Runnable {
+//
+//            while (i < 100){
+//                i += 5
+//                handler.post(Runnable {
+//                    progressBar!!.progress = i
+////                    webViewProcess()
+//                })
+//                try {
+//                    Thread.sleep(100)
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//        }).start()
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
