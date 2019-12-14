e57c894baf7d980becd4ca9476fb64b422013722
sinbad <imufun2@gmail.com>
2019-12-14 17:18:10 +0600

    Initial commit

---
+++ app/src/main/java/com/devsloop/recharge/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.devsloop.recharge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webView)
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl("https://gp.portwallet.com/")
+    }
+}
